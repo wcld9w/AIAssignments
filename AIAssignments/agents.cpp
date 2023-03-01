6755513834d2345a19130bfc7de258ad6c4e03b3
wcld9w <wcld9w@umsystem.edu>
2023-03-01 15:44:25 -0600

    fiusafjasdjfjaklsdkljfadslkhfljgasdknagdskjg

--- AIAssignments/agents.cpp
+++ AIAssignments/agents.cpp
@@ -35,8 +35,8 @@ vector<string> randomPlacement(puzzleBoard inputBoard)
   return workingBoard;
 }
 
-//Used to place the cows onto the board
-vector<string> BFSPlaceCords(vector<cords> grassTileList, vector<int> cowPos, vector<string> workingBoard)
+//Used to place the cows onto the board for easy scoring without editing the existing board
+vector<string> PlaceCords(vector<cords> grassTileList, vector<int> cowPos, vector<string> workingBoard)
 {
   vector<string> temp = workingBoard;
   //For each cow we need to place
@@ -49,6 +49,7 @@ vector<string> BFSPlaceCords(vector<cords> grassTileList, vector<int> cowPos, ve
   return temp;
 }
 
+//This is 
 vector<string> BFSPlacementTrueTargetScore(puzzleBoard inputBoard, int score)
 {
   //Variable declarations
@@ -61,6 +62,8 @@ vector<string> BFSPlacementTrueTargetScore(puzzleBoard inputBoard, int score)
   //Variables to assist in breadth first search
   vector<int> cowsList(inputBoard.getHayBales(), 0);;
   int currentCowNum = 1;
+  //Variable to store the size of the board to reduce calls
+  int boardSize = inputBoard.getBoardSize()
   //Get a list of all the open grass tiless
   grassTileList = inputBoard.getGrassTileList();
 
@@ -68,43 +71,58 @@ vector<string> BFSPlacementTrueTargetScore(puzzleBoard inputBoard, int score)
   while (true)
   {
     //Check scores of current cow placement
-    if (currentScore >= 7)
+      //First we generate a temporary list to store which cows we care about
+    vector<cords> tempList;
+    for (int i = 0; i < currentCowNum; i++)
     {
-      ////////////////////
-      //We can just return here
-      ////////////////////
+      tempList.push_back(cowsList[i]);
     }
-    //Move cows
-      //If can move cow move cow
+      //Then we check the scores without editing the working board
+    int tempScore = getScore(BFSPlaceCords(grassTileList, tempList, workingBoard), boardSize);
+      //If the score is equal to or exceeds our desired amount 
+    if (tempScore >= score)
+    {
+      //We actually place the cows and return the board
+      for (int i = 0; i < currentCowNum; i++)
+      {
+        workingBoard[grassTileList[cowsList[i]].x][grassTileList[cowsList[i]].y] = 'C';
+      }
+      return workingBoard;
+    }
     //This gets the first cow in our list that can be moved
     int canMove = -1;
     for (int i = 0; i < currentCowNum; i++)
     {
       if (cowsList[0] != grassTileList.size() - (1 + i)) canMove = true;
     }
-    //if its -1, we need to increase cow count
+    //if its -1, we need to increase cow count, as we can not move any cows
     if(canMove == -1)
     {
+      //This increases the cows
       currentCowNum += 1;
+      //This is error handling if the number of cows exceeds maximum, prevents the code from going infinite as well if the input is not possible
+      if (currentCowNum > maxNumOfCows)
+      {
+        cout << "Error, the given board can not have the needed score";
+        return workingBoard;
+      }
+      //And this resets the cows to the correct location
       for (int i = 0; i < currentCowNum; i++)
       {
         cowsList[i] = currentCowNum - 1;
       }
-    //Else we just need to move the cow forward, and cows previous to infront of it
     }
+    //Else we just need to move the cow found forward, and cows previous to infront of it
     else
     {
+      //For the cow that we can move, we move it forward one tile
       cowsList[canMove] += 1;
+      //And for all following cows the move them to follow the previous cow
       for (int i = canMove - 1; i >= 0; i--)
       {
         cowsList[i] = cowsList[i+1] + 1;
       }
     }
-      
-  /*
-  
-
-    */
   }
 
 
