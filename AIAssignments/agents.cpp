4f43699aff3bb2c5d2086942104c09dc2c532d8a
wcld9w <wcld9w@umsystem.edu>
2023-03-21 14:22:18 -0500

    LESGOOOOOOOOOOOO

--- AIAssignments/agents.cpp
+++ AIAssignments/agents.cpp
@@ -3,22 +3,35 @@
 
 using namespace std;
 
+
+
+vector<string> PlaceCords(vector<cords> grassTileList, vector<int> cowPos, vector<string> workingBoard)
+{
+  for (int i = 0; i < cowPos.size(); i++)
+  {
+    workingBoard[grassTileList[cowPos[i]].x][grassTileList[cowPos[i]].y] = 'C';
+  }
+  return workingBoard;
+}
+
+
+//Places cows randomly
 vector<string> randomPlacement(puzzleBoard inputBoard)
 {
   //Variable declarations
     //Get the number of cows we have to place
   int numOfCows = inputBoard.getHayBales();
-    //Input a board that we can edit without messing with the input board
+  //Input a board that we can edit without messing with the input board
   vector<string> workingBoard = inputBoard.getBoard();
-    //A variable to hold all the tiles we consider open
+  //A variable to hold all the tiles we consider open
   vector<cords> grassTileList;
 
   //Get a list of all the open grass tiless
   grassTileList = inputBoard.getGrassTileList();
-  
+
   //Pick tiles until we no longer need any
   int numOfCowsRemaining = numOfCows;
-    //While we can place cows
+  //While we can place cows
   while (numOfCowsRemaining != 0)
   {
     //Pick a random number from 0 to n, where n is equal to the number of grass tiles remaining
@@ -29,37 +42,13 @@ vector<string> randomPlacement(puzzleBoard inputBoard)
     //remove the tile from the list so we don't write multiple cows to the same tile
     grassTileList.erase(grassTileList.begin() + pickedCordNum);
     //We have placed a cow remove the tile
-    numOfCowsRemaining--; 
+    numOfCowsRemaining--;
   }
 
   return workingBoard;
 }
 
-//Used to place the cows onto the board for easy scoring without editing the existing board
-vector<string> PlaceCords(vector<cords> grassTileList, vector<int> cowPos, vector<string> workingBoard)
-{
-  vector<string> temp = workingBoard;
-  //For each cow we need to place
-  for (int i = 0; i < cowPos.size(); i++)
-  {
-    //Places the cow where it needs to go
-      //For each cow (i) use its int in the grass tile list to get associated cordinates
-    temp[grassTileList[cowPos[i]].x][grassTileList[cowPos[i]].y] = 'C';
-  }
-  return temp;
-}
-
-
-vector<string> BFSPlaceCords(vector<cords> grassTileList, vector<int> cowPos, vector<string> workingBoard)
-{
-  for (int i = 0; i < cowPos.size(); i++)
-  {
-    workingBoard[grassTileList[cowPos[i]].x][grassTileList[cowPos[i]].y] = 'C';
-  }
-  return workingBoard;
-}
-
-
+//Places cows according to BFS principles
 vector<string> BFSPlacementTrueTargetScore(puzzleBoard inputBoard, int score)
 {
   //Variable declarations
@@ -86,7 +75,7 @@ vector<string> BFSPlacementTrueTargetScore(puzzleBoard inputBoard, int score)
       tempList.push_back(cowsList[i]);
     }
     //Then we check the scores without editing the working board
-    int tempScore = getScore(BFSPlaceCords(grassTileList, tempList, workingBoard), boardSize);
+    int tempScore = getScore(PlaceCords(grassTileList, tempList, workingBoard), boardSize);
     //If the score is equal to or exceeds our desired amount 
     if (tempScore >= score)
     {
@@ -137,5 +126,122 @@ vector<string> BFSPlacementTrueTargetScore(puzzleBoard inputBoard, int score)
   return workingBoard;
 }
 
+//Places cows according to ID-DFS
+vector<string> iterativeDeepDepthFirstSearch(puzzleBoard inputBoard, int targetScore)
+{
+  //Variable declarations
+   //Get the number of cows we have to place
+  int maxNumOfCows = inputBoard.getHayBales();
+  //Input a board that we can edit without messing with the input board
+  vector<string> workingBoard = inputBoard.getBoard();
+  //A variable to hold all the tiles we consider open
+  vector<cords> grassTileList = inputBoard.getGrassTileList();;
+  //Variable to store the size of the board to reduce calls
+  int boardSize = inputBoard.getBoardSize();
+  //Variable to hold the current depth
+  int curDepth = 1;
+  //This is where we get absolutly bonkers
+    //-1 indicates that the cow is not placed
+  vector<int> cowsList(inputBoard.getHayBales(), -1);
+  int lastDepth = 0;
+  //Iterative Deepening depth first search
 
+  while (true)
+  {
+    int lastCow = -1;
+    for (int i = 0; i < curDepth; i++)
+    {
+      if (cowsList[i] != -1) lastCow = i;
+    }
+    //Compare current cow num to desired depth
+    if (lastCow+1 < curDepth) //This looks wacky, but because lastCow starts at 0 (array indices) we need this
+    {
+      //if we need to go deeper
+      //add a cow to the list
+      //This ternary operation is used to inline deal with the discrepency of first cow vs adding all subsiquent cows
+      cowsList[lastCow + 1] = lastCow == -1 ? 0 : cowsList[lastCow] + 1;
+        //We can always add cows to end because we check for this when we go backwards in depth(VERY IMPORTANT, this allows us to optimize the checks somewhat skipping some repeat states)
+    }
+    else
+    {
+      //if we need to stay at current level (deepest)
+      if (cowsList[lastCow] == grassTileList.size() - 1) //This checks if the last cow is able to be incremented
+      {
+        //If we need to go back
+         //Loop to next location in DFS (the next cow that can be incremented, this is a logic replacement for function calls ending recusively backwards)
+        int lastValidLocation = -1;
+        for (int i = lastCow; i >= 0; i--)
+        {
+          if (cowsList[i] != grassTileList.size() - (1 + (lastCow-i)))
+          {
+            lastValidLocation = i;
+            break;
+          }
+        }
 
+        if (lastValidLocation != -1)//We can go back to a certain cow
+        {
+          //Increment the cow that we can move
+          cowsList[lastValidLocation]++;
+          //Check if we can place all following cows, else we can skip the rest of this loop
+
+            //Reset all cows following the one we can move
+            for (int i = lastValidLocation+1; i < curDepth; i++)
+            {
+              cowsList[i] = -1;
+            }
+          
+        }
+        if (lastValidLocation == -1)
+        {
+          //Check if we can increase the depth without going over the allowed number of cows
+          if (curDepth + 1 > maxNumOfCows)
+          {
+            //If we cant then exit
+            cout << "Error, not enough cow locations to get target score in given board, returning empty board";
+            return workingBoard;
+          }
+          else
+          {
+            //Else we reset the cow list
+            for (int i = 0; i < cowsList.size(); i++)
+            {
+              cowsList[i] = -1;
+            }
+            //And increase the depth
+            curDepth++;
+          }
+
+        }
+        
+      }
+      else
+      {
+        //If we don't need to do anything other than move to the next state we move to the next state
+        cowsList[lastCow] += 1;
+      }
+
+    }
+
+    //This generates the list of all cows that we can place
+    vector<int> tempList;
+    for (int i = 0; i < cowsList.size(); i++)
+    {
+      if (cowsList[i] != -1) tempList.push_back(cowsList[i]);
+    }
+    //Then we check the scores without editing the working board
+    int tempScore = getScore(PlaceCords(grassTileList, tempList, workingBoard), boardSize);
+    //If the score is equal to or exceeds our desired amount 
+    if (tempScore >= targetScore)
+    {
+      //We actually place the cows and return the board
+      for (int i = 0; i < tempList.size(); i++)
+      {
+        workingBoard[grassTileList[cowsList[i]].x][grassTileList[cowsList[i]].y] = 'C';
+      }
+      return workingBoard;
+    }
+  }
+  //This return isn't ever reached, but included beacuse C++ throws a fit if no return statement at the end of the function I think, IDK i never checked because its easy to add one line instead of testing
+  return workingBoard;
+}
--- AIAssignments/agents.h
+++ AIAssignments/agents.h
@@ -8,6 +8,7 @@ using namespace std;
   #include<cstdlib>
   #include "gameFile.h"
   vector<string> randomPlacement(puzzleBoard inputBoard);
-  vector<string> BFSPlaceCords(vector<cords> grassTileList, vector<int> cowPos, vector<string> workingBoard); 
+  vector<string> PlaceCords(vector<cords> grassTileList, vector<int> cowPos, vector<string> workingBoard); 
   vector<string> BFSPlacementTrueTargetScore(puzzleBoard inputBoard, int score = 7);
+  vector<string> iterativeDeepDepthFirstSearch(puzzleBoard inputBoard, int targetScore = 7);
 #endif
